7dbb31eb0702baf857d1269364c09d7b68e88d26
VCruzSant <sviniantiago@gmail.com>
2024-06-21 09:03:09 -0300

    Config django models and media folder

--- recipes/models.py
+++ recipes/models.py
@@ -18,6 +18,9 @@ from django.db import models
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
@@ -39,3 +42,6 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
