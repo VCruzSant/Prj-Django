d45b71f7977ec333e63e259098c354ff34f4f825
VCruzSant <sviniantiago@gmail.com>
2024-06-21 12:38:53 -0300

    list view recipes adjustment with orm Models

--- recipes/urls.py
+++ recipes/urls.py
@@ -6,6 +6,12 @@ app_name = 'recipes'
 
 urlpatterns = [
     path('', views.home, name="home"),
+
+    path('recipes/category/<int:category_id>/',
+         views.category,
+         name="category"
+         ),
+
     path('recipes/<int:id>/', views.recipe, name="recipe"),
 
 ]
