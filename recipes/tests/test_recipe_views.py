1a74f6205f9007df1f675c76ab05db06bcbe67cd
VCruzSant <sviniantiago@gmail.com>
2024-06-24 13:27:51 -0300

    test templte content

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -24,3 +24,7 @@ class RecipeViewsTest(TestCase):
     def test_recipe_home_view_loads_correct_template(self):
         response = self.client.get(reverse('recipes:home'))
         self.assertTemplateUsed(response, 'recipes/pages/home.html')
+
+    def test_recipe_home_template_shows_recipe_not_found_if_no_recipe(self):
+        response = self.client.get(reverse('recipes:home'))
+        self.assertIn('No Recipes Found', response.content.decode('utf-8'))
