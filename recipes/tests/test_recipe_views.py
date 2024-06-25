9beedd65e77ddfa0292497f2d701b40f64becc03
VCruzSant <sviniantiago@gmail.com>
2024-06-25 13:47:57 -0300

    test context of category and home

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -1,3 +1,5 @@
+# from unittest import skip
+
 from django.urls import reverse, resolve
 
 from recipes import views
@@ -5,9 +7,12 @@ from .test_recipe_base import RecipeTestBase
 
 
 class RecipeViewsTest(RecipeTestBase):
+    # @skip('WIP(work in progress)')
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
+        # Force test fail to implement in future
+        # self.fail()
 
     def test_recipe_home_view_return_status_code_200(self):
         response = self.client.get(reverse('recipes:home'))
@@ -38,6 +43,15 @@ class RecipeViewsTest(RecipeTestBase):
             reverse('recipes:category', kwargs={'category_id': 1000})
         )
         self.assertEqual(response.status_code, 404)
+
+    def test_recipe_category_context_loads_recipes(self):
+        self.make_recipe()
+        response = self.client.get(
+            reverse('recipes:category', kwargs={'category_id': 1})
+        )
+        response_recipes = response.context['recipes']
+
+        self.assertEqual(response_recipes[0].title, 'Recipe Title test')
 
     def test_recipe_detail_view_function_is_correct(self):
         view = resolve(reverse('recipes:recipe', kwargs={'id': 1}))
