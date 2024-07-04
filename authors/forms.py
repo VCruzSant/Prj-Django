6ad0488e40e2daf251aaba10beab4e7deaa018c3
VCruzSant <sviniantiago@gmail.com>
2024-07-04 11:35:57 -0300

    config form fields

--- authors/forms.py
+++ authors/forms.py
@@ -2,7 +2,29 @@ from django import forms
 from django.contrib.auth.models import User
 
 
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
+
+
+def add_placeholder(field, placeholder_val):
+    add_attr(field, 'placeholder', placeholder_val)
+
+
 class RegisterForm(forms.ModelForm):
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={
+                'placeholder': 'Confirm your password'
+            }
+        ),
+        label='Confirm Password',
+        error_messages={
+            'required': 'Password must not be empty'
+        }
+
+    )
+
     class Meta:
         model = User
         fields = [
@@ -12,3 +34,32 @@ class RegisterForm(forms.ModelForm):
             'email',
             'password',
         ]
+        # exclude = ['first_name']
+
+        labels = {
+            'username': 'Username',
+            'first_name': 'First name',
+            'last_name': 'Last name',
+            'email': 'E-mail',
+            'password': 'Password',
+        }
+
+        help_texts = {
+            'email': 'Must be valid'
+        }
+
+        error_messages = {
+            'username': {
+                'required': 'This field must not be empty',
+                'invalid': 'This field is invalid,'
+            }
+        }
+
+        widgets = {
+            'first_name': forms.TextInput(attrs={
+                'placeholder': 'Type your username here'
+            }),
+            'password': forms.PasswordInput(attrs={
+                'placeholder': 'Type your password here'
+            })
+        }
