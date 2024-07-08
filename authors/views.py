584396f7cc8db8964f58e0a75ecbe3b2d9b5d9ec
VCruzSant <sviniantiago@gmail.com>
2024-07-08 16:56:05 -0300

    login create view

--- authors/views.py
+++ authors/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import Http404
 from django.contrib import messages
 from django.urls import reverse
+from django.contrib.auth import authenticate, login
 
 from .forms import RegisterForm, LoginForm
 
@@ -49,4 +50,26 @@ def login_view(request):
 
 
 def login_create(request):
-    return render(request, 'authors/pages/login.html')
+    if not request.POST:
+        return Http404()
+
+    form = LoginForm(request.POST)
+    login_url = reverse('authors:login')
+
+    if form.is_valid():
+        authenticated_user = authenticate(
+            username=form.cleaned_data.get('username', ''),
+            password=form.cleaned_data.get('password', '')
+        )
+
+        if authenticated_user is not None:
+            messages.success(request, 'Your are logged in')
+            login(request, authenticated_user)
+
+        else:
+            messages.error(request, 'invalid credentials')
+
+    else:
+        messages.error(request, 'invalid username or password')
+
+    return redirect(login_url)
