d6f3971bb6ea5072d0bed12ff4b2069100b6960b
VCruzSant <sviniantiago@gmail.com>
2024-07-10 12:15:56 -0300

    using functional test marker

--- tests/functional_tests/recipes/test_recipe_home_page.py
+++ tests/functional_tests/recipes/test_recipe_home_page.py
@@ -1,7 +1,9 @@
+import pytest
 from selenium.webdriver.common.by import By
 from .base import RecipeBaseFunctionalTest
 
 
+@pytest.mark.functional_test
 class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTest):
     def test_recipe_home_page_without_recipe_not_found(self):
         self.browser.get(self.live_server_url)
