f75dcf9d12c581829e31a947ba12e336426f2562
VCruzSant <sviniantiago@gmail.com>
2024-07-15 10:50:10 -0300

    config django admin for recipes

--- recipes/admin.py
+++ recipes/admin.py
@@ -12,4 +12,16 @@ admin.site.register(Category, CategoryAdmin)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    ...
+    list_display = (
+        'id', 'title', 'created_at', 'is_published', 'category', 'author'
+    )
+    list_display_links = 'id', 'title',
+    search_fields = 'id', 'title', 'description', 'author', 'preparation_step'
+    list_filter = (
+        'category', 'author', 'is_published', 'preparation_step_is_html'
+    )
+    list_per_page = 10
+    list_editable = 'is_published',
+    prepopulated_fields = {
+        "slug": ('title',)
+    }
