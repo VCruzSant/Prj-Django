6a78605bcb14bcb998618de13004c9e220ea7a7d
VCruzSant <sviniantiago@gmail.com>
2024-07-15 11:31:57 -0300

    create dashboard recipe list

--- authors/views.py
+++ authors/views.py
@@ -6,6 +6,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 
 from .forms import RegisterForm, LoginForm
+from recipes.models import Recipe
 
 # Create your views here.
 
@@ -95,4 +96,14 @@ def logout_view(request):
 
 @login_required(login_url='authors:login', redirect_field_name='next')
 def dashboard_view(request):
-    return render(request, 'authors/pages/dashboard.html')
+    recipes = Recipe.objects.filter(
+        is_published=False,
+        author=request.user
+    )
+    return render(
+        request,
+        'authors/pages/dashboard.html',
+        {
+            "recipes": recipes
+        }
+    )
