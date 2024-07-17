a0a4173449547eda3846df4b6acbc55e1825246d
VCruzSant <sviniantiago@gmail.com>
2024-07-17 10:03:38 -0300

    CBV: convert view to class

---
+++ authors/views/dashboard_recipe.py
@@ -0,0 +1,59 @@
+from django.http.response import Http404
+from django.views import View
+from django.contrib import messages
+from django.shortcuts import redirect, render
+from django.urls import reverse
+
+from recipes.models import Recipe
+from authors.forms.recipe_form import AuthorRecipeForm
+
+
+class DashboardRecipe(View):
+    def get_recipe(self, id):
+        recipe = None
+
+        if id:
+            recipe = Recipe.objects.filter(
+                is_published=False,
+                author=self.request.user,
+                id=id
+            ).first()
+
+        if not recipe:
+            raise Http404
+
+        return recipe
+
+    def render_recipe(self, form):
+        return render(
+            self.request,
+            'authors/pages/dashboard_recipe.html',
+            {"form": form}
+        )
+
+    def get(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(instance=recipe)
+        return self.render_recipe(form)
+
+    def post(self, request, id):
+        recipe = self.get_recipe(id)
+        form = AuthorRecipeForm(
+            request.POST or None,
+            files=request.FILES or None,
+            instance=recipe  # type: ignore
+        )
+
+        if form.is_valid():
+            recipe = form.save(commit=False)
+
+            recipe.author = request.user
+            recipe.preparation_steps_is_html = False
+            recipe.is_published = False
+
+            recipe.save()
+            messages.success(request, 'Your recipe has been saved')
+            return redirect(
+                reverse('authors:dashboard_recipe_edit', args=(id,))
+            )
+        return self.render_recipe(form)
