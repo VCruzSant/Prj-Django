9052c892b86aaa88364773dd63ee8ad6d45d8e2f
VCruzSant <sviniantiago@gmail.com>
2024-08-05 16:52:54 -0300

    recapping ways to manage objects

--- recipes/views/theory.py
+++ recipes/views/theory.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
+from recipes.models import Recipe
 
 
 def theory(request, *args, **kwargs):
+    recipes = Recipe.objects.get(id=20)
+    # recipes = Recipe.objects.all()
+    # recipes = recipes \
+    #     .order_by('-id') \
+    #     .first()
+
+    context = {
+        'recipes': recipes
+    }
+
     return render(
-        request, 'recipes/pages/theory.html'
+        request, 'recipes/pages/theory.html', context=context
     )
