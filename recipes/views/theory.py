d3502c05d07b0125dee417257e8c645bf97391e8
VCruzSant <sviniantiago@gmail.com>
2024-08-07 14:52:49 -0300

    optimizing queries by specifying fields

--- recipes/views/theory.py
+++ recipes/views/theory.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render
 from recipes.models import Recipe
+# from django.db.models import Q, F
 
 
 def theory(request, *args, **kwargs):
-    recipes = Recipe.objects.get(id=20)
+    # recipes = Recipe.objects.filter(
+    #     Q(title__icontains='recipe',
+    #       id__gt=5,
+    #       is_published=True,) |
+    #     Q(
+    #         id__gt=100
+    #     )
+    # )[:10]
+
     # recipes = Recipe.objects.all()
     # recipes = recipes \
     #     .order_by('-id') \
     #     .first()
+
+    # recipes = Recipe.objects.filter(
+    #     id=F('author__id'),
+    # )[:10]
+
+    recipes = Recipe.objects \
+        .values('id', 'title', 'author__username')[10:20]
 
     context = {
         'recipes': recipes
