967f83b12c5bd1f211b0c652ce036de4ecb4e7eb
VCruzSant <sviniantiago@gmail.com>
2024-08-08 15:40:52 -0300

    create tag model

---
+++ tag/models.py
@@ -0,0 +1,37 @@
+import string
+from random import SystemRandom
+
+from django.db import models
+from django.utils.text import slugify
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+
+# Create your models here.
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+
+    # Generic Relation
+    # Model:
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    # Model id
+    object_id = models.CharField()
+    # Relation Generic Field
+    content_object = GenericForeignKey('content_type', 'object_id')
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            rand_letters = ''.join(
+                SystemRandom().choices(
+                    string.ascii_letters + string.digits, k=5
+                )
+            )
+            self.slug = slugify(f'{self.name}-{rand_letters}')
+
+        return super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
